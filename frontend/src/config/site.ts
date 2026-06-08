7eb4b57158ae7c8ba25e47676ce2f875603e6169
I-techdigitals <business.itechdigitals@gmail.com>
2026-06-08 02:41:17 -0700

    done

--- frontend/src/config/site.ts
+++ frontend/src/config/site.ts
@@ -2,3 +2,6 @@
 export const COMPANY_PHONE_DISPLAY = "+96599402446";
 export const COMPANY_PHONE_E164 = "96599402446";
 export const WHATSAPP_URL = `https://wa.me/${COMPANY_PHONE_E164}`;
+
+export const LINKEDIN_URL = "https://www.linkedin.com/company/itechkw";
+export const INSTAGRAM_URL = "https://www.instagram.com/itech.kw";
